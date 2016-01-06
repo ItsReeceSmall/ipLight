da2894d5d11285f161002f7462ddb45a7101f889
Reece Small <rsmall@salisbury6c.ac.uk>
2016-01-06 12:40:57 +0000

    Update ipLight.py

--- ipLight.py
+++ ipLight.py
@@ -1,38 +1,9 @@
 import RPi.GPIO as gpio
-import sys
-import os
-import socket
+import sys, os, socket
 import ipaddress
 
-if os.name != "nt":
-    import fcntl
-    import struct
+print (ipaddress.get_lan_ip() + ' is the ip address of this system.')
 
-    def get_interface_ip(ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
-                                ifname[:15]))[20:24])
+ip = ipaddress.get_lan_ip()
 
-def get_lan_ip():
-    ip = socket.gethostbyname(socket.gethostname())
-    if ip.startswith("127.") and os.name != "nt":
-        interfaces = [
-            "eth0",
-            "eth1",
-            "eth2",
-            "wlan0",
-            "wlan1",
-            "wifi0",
-            "ath0",
-            "ath1",
-            "ppp0",
-            ]
-        for ifname in interfaces:
-            try:
-                ip = get_interface_ip(ifname)
-                break
-            except IOError:
-                pass
-    return ip
-
-print ipaddress.get_lan_ip()
+print (ip)
